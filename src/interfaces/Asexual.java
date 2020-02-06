25de7280d85491914ebb4b19cb03ea87cb5aac37
Max-le <tftic34814555+Max-le@users.noreply.github.com>
2020-02-06 16:53:12 +0100

    comments on interface

--- src/interfaces/Asexual.java
+++ src/interfaces/Asexual.java
@@ -4,5 +4,6 @@ package interfaces;
 import models.Weed;
 
 public interface Asexual {
+    // A Weed should be able to return an array of weeds.
     Weed[] createChild();
 }
--- src/interfaces/Sexual.java
+++ src/interfaces/Sexual.java
@@ -3,5 +3,7 @@ package interfaces;
 import models.Fish;
 
 public interface Sexual {
+        //A fish, with it's mate as a parameter, shroud be abbe to return a new fish.
+        //Can return null if reproduction was not possible for mose reason ( not compatible )
         Fish createChild(Fish mate);
 }
